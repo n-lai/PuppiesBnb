fc6918ccd74d60398a50882a7daa7012e680550c
n-lai <nlai@ucdavis.edu>
2016-06-30 14:15:53 -0700

    Fix CSS styling

---
+++ frontend/components/puppy_map.jsx
@@ -0,0 +1,13 @@
+const React = require('react');
+
+const PuppyMap = React.createClass({
+  render() {
+    return (
+      <div className='map', ref='map'>
+
+      </div>
+    );
+  }
+});
+
+module.exports = PuppyMap;
